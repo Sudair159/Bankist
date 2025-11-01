f948222f181a5e76a74555e2720b7878230215be
Sudais Bukhari <sudais159bukhari@outlook.com>
2025-11-01 13:06:06 +0500

    Refactor account data structure and UI elements

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const account4 = {
     '2023-06-22T23:36:17.929Z',
     '2023-06-22T23:36:17.929Z',
   ],
-  currency: 'PKR',
+  currency: 'GBP',
   locale: 'en-US',
 };
 
@@ -459,6 +459,7 @@ const deleteAccount = function (e) {
   inputClosePin.blur();
 };
 btnClose.addEventListener('click', deleteAccount);
+
 
 
 
